d1f52c28604c4ed59638494a4d2de5171787d929
DP3AMizernuks <>
2021-06-01 12:19:15 +0000

    Add a new buttons, Button "Flight work".

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ def index():
 def about():
   return render_template("about.html")
 
+@app.route('/flights')
+def flights():
+  return render_template("flights.html")
+
 
 app.run(host='0.0.0.0', port=8080)
